2144ba29b18bf91c0b4b22b77f6ced594694102c
Jack Hsu <jack.hsu@gmail.com>
2024-10-04 08:57:37 -0400

    fix test

--- apps/blog/src/app/app.spec.tsx
+++ apps/blog/src/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
-    expect(getByText(/Welcome blog/gi)).toBeTruthy();
+    expect(getByText(/Blog/gi)).toBeTruthy();
   });
 });
